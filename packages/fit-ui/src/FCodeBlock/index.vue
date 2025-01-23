aafa80736766be1645cb18d34fe0a49a39255384
geniusmanyxh <geniusmanyxh@gmail.com>
2025-01-24 04:22:01 +0800

    build: 重构项目配置

--- packages/fit-ui/src/FCodeBlock/index.vue
+++ packages/fit-ui/src/FCodeBlock/index.vue
@@ -1,339 +1,349 @@
 <template>
-    <div ref="codeElRef" class="codeBox" :class="{isFullScreen:isFullScreen}" :data-theme='theme'>
-      <div class="codeBox-header">
-        <div class="codeBox-header-title">{{ title }}</div>
-        <div class="codeBox-header-tools">
-          
-            <span ref="copyButton">
-              <f-icon icon="copy"></f-icon>
-            </span>
-            <f-icon class="tools-item" icon="baseline-code" style="font-size: 20px" @click.stop="hanldeViewSourceCode"></f-icon>
-            <f-icon class="tools-item" :icon="!isFullScreen?'baseline-fullscreen':'baseline-fullscreen-exit'" @click.stop="toggleFullScreen"></f-icon>
-          
-        </div>
+  <div ref="codeElRef" class="codeBox" :class="{ isFullScreen: isFullScreen }" :data-theme='theme'>
+    <div class="codeBox-header">
+      <div class="codeBox-header-title">{{ title }}</div>
+      <div class="codeBox-header-tools">
+
+        <span ref="copyButton">
+          <f-icon icon="copy"></f-icon>
+        </span>
+        <f-icon class="tools-item" icon="baseline-code" style="font-size: 20px"
+          @click.stop="hanldeViewSourceCode"></f-icon>
+        <f-icon class="tools-item" :icon="!isFullScreen ? 'baseline-fullscreen' : 'baseline-fullscreen-exit'"
+          @click.stop="toggleFullScreen"></f-icon>
+
       </div>
-  
-      <Transition
-        appear
-        name="fade-slide"
-        @before-enter="beforeEnter"
-        @enter="enter"
-        @after-enter="afterEnter"
-        @before-leave="beforeLeave"
-        @leave="leave"
-        @after-leave="afterLeave"
-      >
-        <div v-show="isShowCode" class="content">
-          <span class="lang">{{ lang }}</span>
-          <div v-html="highlightedCode" :class="`language-${lang}`"></div>
-        </div>
-      </Transition>
-  <template v-if="!isFullScreen">
-    <div v-show="isShowCode" class="code-hide" @click.stop="hanldeViewSourceCode">
+    </div>
+
+    <Transition appear name="fade-slide" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter"
+      @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave">
+      <div v-show="isShowCode" class="content">
+        <span class="lang">{{ lang }}</span>
+        <div v-html="highlightedCode" :class="`language-${lang}`"></div>
+      </div>
+    </Transition>
+    <template v-if="!isFullScreen">
+      <div v-show="isShowCode" class="code-hide" @click.stop="hanldeViewSourceCode">
         <f-icon icon="caret-up"></f-icon>隐藏代码
       </div>
-  </template>
-     
-    </div>
-  
-  </template>
-  
-  <script setup lang="ts">
-  import FIcon from '@/FIcon';
-  import { ref, onMounted, watch, onBeforeUnmount,nextTick } from 'vue';
-  import { createHighlighterCore } from 'shiki/core';
-  import githubLight from 'shiki/themes/github-light.mjs';
-  import ClipboardJS from 'clipboard'; // 引入 clipboard.js
-  import { useMessage } from '@utils/tsHooks/useMessage'
-  import { toggleFull,getFullEl } from 'tj-jstools'
-  
-  defineOptions({ name: 'FCodeBlock', inheritAttrs: false, })
-  
-  // Props
-  const props = defineProps({
-    title: {
-      type: String,
-      default: '代码示例',
-    },
-    code: String,
-    lang: {
-      type: String,
-      default: 'js', // 默认语言 js,可选：js,html,ts,css
-    },
-    showCode: {
-      type: Boolean,
-      default: false,
-    },
-    theme: {
-      type: String,
-      default: '', // 默认主题  light 可选：light,dark
-    },
-  });
-  
-  const $MSG = useMessage();
-  // Data
-  const highlightedCode = ref('');
-  const isShowCode = ref(false);
-  const isFullScreen = ref(false);
-  const highlighter = ref();
-  const codeElRef = ref(null);
-  const copyButton = ref(null); // 绑定按钮
-  let clipboardInstance:any = null;
-  
-  // Function to highlight code
-  const highlight = async () => {
-    // 初始化高亮器时，先加载主题和语言
-    highlighter.value = await createHighlighterCore({
-      themes: [githubLight, async () => await import('shiki/themes/github-dark.mjs')],
-      langs: [
-        import('shiki/langs/js.mjs'),
-        async () => await import('shiki/langs/html.mjs'),
-        async () => await import('shiki/langs/ts.mjs'),
-        async () => await import('shiki/langs/css.mjs'),
-      ],
-      loadWasm: async () => await import('shiki/wasm'),
-    });
-  
-    changeHighlightedCode();
-  };
-  
-  const changeHighlightedCode = () => {
-    // 使用渲染函数生成 HTML
-    highlightedCode.value = highlighter.value.codeToHtml(props.code, {
-      lang: props.lang,
-      // theme: 'github-dark',
-      theme: props.theme === 'dark' ? 'github-dark' : 'github-light',
-    });
-  };
-  
-  const hanldeViewSourceCode = () => {
-    isShowCode.value = !isShowCode.value;
-  };
-  const beforeEnter = (el: Element) => {
-    // 初始状态，设置高度为0
-    (el as HTMLElement).style.height = '0';
-    (el as HTMLElement).style.opacity = '0';
-    (el as HTMLElement).style.overflow = 'hidden'; // 确保内容在过渡期间不会超出容器
-  };
-  
-  const enter = (el: Element, done: () => void) => {
-    const height = el.scrollHeight; // 获取内容的实际高度
-    (el as HTMLElement).style.transition = 'height 0.6s ease, opacity 0.6s ease';
-    (el as HTMLElement).style.height = `${height}px`; // 逐渐增加到实际高度
-    (el as HTMLElement).style.opacity = '1';
-  
-    el.addEventListener('transitionend', done, { once: true });
-  };
-  
-  const afterEnter = (el: Element) => {
-    (el as HTMLElement).style.height = 'auto'; // 过渡结束后恢复到自动高度
-  };
-  
-  const beforeLeave = (el: Element) => {
-    // 在离开之前，将高度设置为内容的实际高度，避免直接收缩为0
-    (el as HTMLElement).style.height = `${el.scrollHeight}px`;
-    (el as HTMLElement).style.opacity = '1';
-  };
-  
-  const leave = (el: Element, done: () => void) => {
-    // 设置为逐渐减少高度并隐藏
-    (el as HTMLElement).style.transition = 'height 0.6s ease, opacity 0.6s ease';
-    (el as HTMLElement).style.height = '0';
-    (el as HTMLElement).style.opacity = '0';
-  
-    el.addEventListener('transitionend', done, { once: true });
-  };
-  
-  const afterLeave = (el: Element) => {
-    (el as HTMLElement).style.height = ''; // 清除内联高度设置，准备下次显示
-  };
-  
-  // Watch for code changes
-  watch(() => props.code, highlight);
+    </template>
 
-  watch(() => props.showCode, (val) => {
-    isShowCode.value = val;
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import FIcon from '@/FIcon';
+import { ref, onMounted, watch, onBeforeUnmount, nextTick } from 'vue';
+import { createHighlighterCore } from 'shiki/core';
+import githubLight from 'shiki/themes/github-light.mjs';
+import githubDark from 'shiki/themes/github-dark.mjs';
+// `shiki/wasm` 包含以 BASE64 字符串内联的 WASM 二进制文件
+import getWasm from "shiki/wasm";
+import langJs from 'shiki/langs/js.mjs';
+import langHtml from 'shiki/langs/html.mjs';
+import langTs from 'shiki/langs/ts.mjs';
+import langCss from 'shiki/langs/css.mjs';
+import ClipboardJS from 'clipboard'; // 引入 clipboard.js
+import { useMessage } from '@utils/tsHooks/useMessage'
+import { toggleFull, getFullEl } from 'tj-jstools'
+
+defineOptions({ name: 'FCodeBlock', inheritAttrs: false, })
+
+// Props
+const props = defineProps({
+  title: {
+    type: String,
+    default: '代码示例',
+  },
+  code: String,
+  lang: {
+    type: String,
+    default: 'js', // 默认语言 js,可选：js,html,ts,css
+  },
+  showCode: {
+    type: Boolean,
+    default: false,
+  },
+  theme: {
+    type: String,
+    default: '', // 默认主题  light 可选：light,dark
+  },
+});
+
+const $MSG = useMessage();
+// Data
+const highlightedCode = ref('');
+const isShowCode = ref(false);
+const isFullScreen = ref(false);
+const highlighter = ref();
+const codeElRef = ref(null);
+const copyButton = ref(null); // 绑定按钮
+let clipboardInstance: any = null;
+
+// Function to highlight code
+const highlight = async () => {
+  // 初始化高亮器时，先加载主题和语言
+  highlighter.value = await createHighlighterCore({
+    themes: [githubLight, githubDark],
+    langs: [
+      langJs,
+      langHtml,
+      langTs,
+      langCss,
+    ],
+    loadWasm: getWasm,
   });
 
-  watch(() => props.theme, changeHighlightedCode);
-  watch(() => props.lang, changeHighlightedCode);
-  
-  // Highlight code on mounted
-  onMounted(() => {
-    highlight();
-    nextTick(() => {
-      if (props.showCode) {
-        setTimeout(() => {
-          isShowCode.value = props.showCode;
-        }, 1000);
-      }
-    });
-  
-    // 将 ClipboardJS 实例绑定到真实按钮上
-    clipboardInstance = new ClipboardJS(copyButton.value as unknown as Element, {
-      text: () => props.code as string, // 返回需要复制的文本
-    });
-  
-    // 监听复制成功事件
-    clipboardInstance.on('success', (e:any) => {
-      $MSG.success('复制成功!')
-      e.clearSelection(); // 清除选中文本
-    });
-  
-    // 监听复制失败事件
-    clipboardInstance.on('error', (e:any) => {
-      $MSG.error('复制失败，请手动复制!')
-    });
+  changeHighlightedCode();
+};
+
+const changeHighlightedCode = () => {
+  // 使用渲染函数生成 HTML
+  highlightedCode.value = highlighter.value.codeToHtml(props.code, {
+    lang: props.lang,
+    // theme: 'github-dark',
+    theme: props.theme === 'dark' ? 'github-dark' : 'github-light',
   });
-  
-  onBeforeUnmount(() => {
-    // 在组件卸载时销毁 ClipboardJS 实例
-    if (clipboardInstance) {
-      clipboardInstance.destroy();
+};
+
+const hanldeViewSourceCode = () => {
+  isShowCode.value = !isShowCode.value;
+};
+const beforeEnter = (el: Element) => {
+  // 初始状态，设置高度为0
+  (el as HTMLElement).style.height = '0';
+  (el as HTMLElement).style.opacity = '0';
+  (el as HTMLElement).style.overflow = 'hidden'; // 确保内容在过渡期间不会超出容器
+};
+
+const enter = (el: Element, done: () => void) => {
+  const height = el.scrollHeight; // 获取内容的实际高度
+  (el as HTMLElement).style.transition = 'height 0.6s ease, opacity 0.6s ease';
+  (el as HTMLElement).style.height = `${height}px`; // 逐渐增加到实际高度
+  (el as HTMLElement).style.opacity = '1';
+
+  el.addEventListener('transitionend', done, { once: true });
+};
+
+const afterEnter = (el: Element) => {
+  (el as HTMLElement).style.height = 'auto'; // 过渡结束后恢复到自动高度
+};
+
+const beforeLeave = (el: Element) => {
+  // 在离开之前，将高度设置为内容的实际高度，避免直接收缩为0
+  (el as HTMLElement).style.height = `${el.scrollHeight}px`;
+  (el as HTMLElement).style.opacity = '1';
+};
+
+const leave = (el: Element, done: () => void) => {
+  // 设置为逐渐减少高度并隐藏
+  (el as HTMLElement).style.transition = 'height 0.6s ease, opacity 0.6s ease';
+  (el as HTMLElement).style.height = '0';
+  (el as HTMLElement).style.opacity = '0';
+
+  el.addEventListener('transitionend', done, { once: true });
+};
+
+const afterLeave = (el: Element) => {
+  (el as HTMLElement).style.height = ''; // 清除内联高度设置，准备下次显示
+};
+
+// Watch for code changes
+watch(() => props.code, highlight);
+
+watch(() => props.showCode, (val) => {
+  isShowCode.value = val;
+});
+
+watch(() => props.theme, changeHighlightedCode);
+watch(() => props.lang, changeHighlightedCode);
+
+// Highlight code on mounted
+onMounted(() => {
+  highlight();
+  nextTick(() => {
+    if (props.showCode) {
+      setTimeout(() => {
+        isShowCode.value = props.showCode;
+      }, 1000);
     }
   });
 
-  const toggleFullScreen = () => {
-    if (!codeElRef.value) return;
-    const codeBox = getFullEl();
-    if (codeBox) {
-      isFullScreen.value = false;
-    } else {
-      isFullScreen.value = true;
-    }
-    toggleFull(codeElRef.value as unknown as Element)
-  };
-  </script>
-  
-  <style lang="scss" scoped>
-  .fade-slide-enter-active,
-  .fade-slide-leave-active {
-    transition: height 0.3s ease, opacity 0.3s ease;
-  }
-  
-  .fade-slide-enter-from,
-  .fade-slide-leave-to {
-    opacity: 0;
-    height: 0;
-  }
-  .isFullScreen {
-    .content {
-      width: 100% !important;
-      height: calc(100% - 44px) !important;
-      overflow: auto !important;
-      scrollbar-width: none; /* Firefox 64+ */
-      -ms-overflow-style: none;  /* IE 和 Edge */
-      &::-webkit-scrollbar {
-        width: 0;
-      }
+  // 将 ClipboardJS 实例绑定到真实按钮上
+  clipboardInstance = new ClipboardJS(copyButton.value as unknown as Element, {
+    text: () => props.code as string, // 返回需要复制的文本
+  });
 
-      .lang{
-        position: fixed !important;
-        top: 50px !important;
-      }
+  // 监听复制成功事件
+  clipboardInstance.on('success', (e: any) => {
+    $MSG.success('复制成功!')
+    e.clearSelection(); // 清除选中文本
+  });
+
+  // 监听复制失败事件
+  clipboardInstance.on('error', (e: any) => {
+    $MSG.error('复制失败，请手动复制!')
+  });
+});
+
+onBeforeUnmount(() => {
+  // 在组件卸载时销毁 ClipboardJS 实例
+  if (clipboardInstance) {
+    clipboardInstance.destroy();
+  }
+});
+
+const toggleFullScreen = () => {
+  if (!codeElRef.value) return;
+  const codeBox = getFullEl();
+  if (codeBox) {
+    isFullScreen.value = false;
+  } else {
+    isFullScreen.value = true;
+  }
+  toggleFull(codeElRef.value as unknown as Element)
+};
+</script>
+
+<style lang="scss" scoped>
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: height 0.3s ease, opacity 0.3s ease;
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  height: 0;
+}
+
+.isFullScreen {
+  .content {
+    width: 100% !important;
+    height: calc(100% - 44px) !important;
+    overflow: auto !important;
+    scrollbar-width: none;
+    /* Firefox 64+ */
+    -ms-overflow-style: none;
+
+    /* IE 和 Edge */
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+
+    .lang {
+      position: fixed !important;
+      top: 50px !important;
     }
   }
-  .codeBox {
-    width: 100%;
-    height: 100%;
-    // padding: 15px 0;
-    box-sizing: border-box;
-    border: 1px solid var(--f-code-outside-border-color);
-    border-radius: 5px;
+}
+
+.codeBox {
+  width: 100%;
+  height: 100%;
+  // padding: 15px 0;
+  box-sizing: border-box;
+  border: 1px solid var(--f-code-outside-border-color);
+  border-radius: 5px;
+  position: relative;
+  background-color: var(--f-code-bg-color);
+
+  ::v-deep(.el-tooltip__trigger) {
+    display: flex;
+  }
+
+  .tooltip {
+    display: flex;
+  }
+
+  .content {
+    overflow: hidden;
+    transition: height 0.6s ease, opacity 0.6s ease;
+    /* 确保动画的平滑性 */
     position: relative;
+    // background-color: #f6f6f7;
+    // background-color: #24292e;
     background-color: var(--f-code-bg-color);
 
-    ::v-deep(.el-tooltip__trigger) {
-      display: flex;
+    ::v-deep(.shiki) {
+      background-color: transparent !important;
     }
-    .tooltip {
-      display: flex;
+
+    div[class*='language-'] {
+      margin-top: 0;
+      margin-bottom: 0;
+      border-radius: 0;
     }
-    .content {
-      overflow: hidden;
-      transition: height 0.6s ease, opacity 0.6s ease; /* 确保动画的平滑性 */
-      position: relative;
-      // background-color: #f6f6f7;
-      // background-color: #24292e;
-      background-color: var(--f-code-bg-color);
-  
-      ::v-deep(.shiki) {
-        background-color: transparent !important;
-      }
-  
-      div[class*='language-'] {
-        margin-top: 0;
-        margin-bottom: 0;
-        border-radius: 0;
-      }
-  
-      .lang {
-        position: absolute;
-        // position: sticky;
-        right: 8px;
-        top: 2px;
-        color: var(--f-code-lang-color);
-        z-index: 2;
-      }
+
+    .lang {
+      position: absolute;
+      // position: sticky;
+      right: 8px;
+      top: 2px;
+      color: var(--f-code-lang-color);
+      z-index: 2;
     }
-  
-    .code-hide {
-      width: 100%;
+  }
+
+  .code-hide {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    height: 44px;
+    box-sizing: border-box;
+    border-top: 1px solid var(--f-code-outside-border-color);
+    background-color: var(--f-code-outside-bg-color);
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    margin-top: -1px;
+    color: var(--f-code-outside-text-color);
+    cursor: pointer;
+    position: sticky;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 10;
+  }
+
+  .codeBox-header {
+    width: 100%;
+    height: 44px;
+    padding: 5px 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid var(--f-code-outside-border-color);
+    background-color: var(--f-code-outside-bg-color);
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    box-sizing: border-box;
+    color: var(--f-code-outside-text-color);
+
+    &-title {
+      font-weight: 600;
+      letter-spacing: 2px;
+    }
+
+    &-tools {
       display: flex;
-      justify-content: center;
+      justify-content: end;
       align-items: center;
+      gap: 10px;
       cursor: pointer;
-      height: 44px;
-      box-sizing: border-box;
-      border-top: 1px solid var(--f-code-outside-border-color);
-      background-color: var(--f-code-outside-bg-color);
-      border-bottom-left-radius: 4px;
-      border-bottom-right-radius: 4px;
-      margin-top: -1px;
-      color: var(--f-code-outside-text-color);
-      cursor: pointer;
-      position: sticky;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      z-index: 10;
-    }
-    .codeBox-header {
-      width: 100%;
-      height: 44px;
-      padding: 5px 15px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid var(--f-code-outside-border-color);
-      background-color: var(--f-code-outside-bg-color);
-      border-top-left-radius: 4px;
-      border-top-right-radius: 4px;
-      box-sizing: border-box;
-      color: var(--f-code-outside-text-color);
-  
-      &-title {
-        font-weight: 600;
-        letter-spacing: 2px;
-      }
-  
-      &-tools {
-        display: flex;
-        justify-content: end;
-        align-items: center;
-        gap: 10px;
-        cursor: pointer;
-  
-        .tools-item {
-          font-size: 20px;
-        }
+
+      .tools-item {
+        font-size: 20px;
       }
     }
   }
-  /* pre {
+}
+
+/* pre {
     padding: 1em;
     overflow: auto;
     border-radius: 5px;
   } */
-  </style>
-  
+</style>
--- packages/fit-ui/vite.config.ts
+++ packages/fit-ui/vite.config.ts
@@ -1,72 +1,52 @@
-///<reference types="vitest"/>
-import { defineConfig } from 'vite'
-import type { UserConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
+import type { ServerOptions } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 import UnoCSS from './config/unocss'
 import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
 import DefineOptions from 'unplugin-vue-define-options/vite'
-
-// import { formatAssetFile,formatChunkFile,formatEntryFile } from './config/formatBuildFileName'
 
 type ExportsType = 'default' | 'named' | 'none' | 'auto'
 
-
 const rollupOptions = {
   external: ['vue', 'vue-router'],
-  input:resolve(__dirname,"./src/entry.ts") ,
+  input: resolve(__dirname, "./src/entry.ts"),
   output: [
-    { 
-      //不用打包成.es.js,这里我们想把它打包成.js
-      
-      assetFileNames: "assets/[name].[ext]",
-      // 指定生成的 entry 文件的名称
-      entryFileNames: "[name].js",
-      // 指定生成的 chunk 文件的名称
-      // chunkFileNames: 'chunks/[name].js',
+    {
       format: 'es',
-      //让打包目录和我们目录对应
-      preserveModules: true,
-      preserveModulesRoot: './',
-      globals: {
-        vue: 'Vue',
-      },
-      exports: 'named' as ExportsType, // 消除默认导出和命名导出的警告
-      //配置打包根目录
       dir: 'dist/es',
-    },
-    { 
-      //不用打包成.es.js,这里我们想把它打包成.js
+      entryFileNames: '[name].js',
+      // chunkFileNames: 'chunks/[name]-[hash].js',
       assetFileNames: "assets/[name].[ext]",
-      // 指定生成的 entry 文件的名称
-      entryFileNames: "[name].js",
-      // 指定生成的 chunk 文件的名称
-      // chunkFileNames: 'chunks/[name].js',
-      format: 'cjs',
-      //让打包目录和我们目录对应
+      globals: { vue: 'Vue' },
+      exports: 'named',
       preserveModules: true,
       preserveModulesRoot: './',
-      globals: {
-        vue: 'Vue',
-      },
-      exports: 'named' as ExportsType, // 消除默认导出和命名导出的警告
-      //配置打包根目录
+      // preserveEntrySignatures: 'allow-extension',
+    },
+    {
+      format: 'cjs',
       dir: 'dist/lib',
-    }
-  ] 
-  
-}
+      entryFileNames: '[name].js',
+      // chunkFileNames: 'chunks/[name]-[hash].js',
+      assetFileNames: "assets/[name].[ext]",
+      globals: { vue: 'Vue' },
+      exports: 'named',
+      preserveModules: true,
+      preserveModulesRoot: './',
+      // preserveEntrySignatures: 'allow-extension',
+    },
+  ],
+} as any;
 
-export const config = {
+export const config: UserConfigExport = {
   define: {
-    // 启用生产环境构建下激活不匹配的详细警告
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
   },
   resolve: {
     alias: {
-      // vue: 'vue/dist/vue.esm-bundler.js',
       '@': resolve(__dirname, 'src'),
       '@utils': resolve(__dirname, 'utils'),
       '@enums': resolve(__dirname, 'enums'),
@@ -75,55 +55,46 @@ export const config = {
     },
   },
   plugins: [
-    vue(), // VUE插件
-    vueJsx(), // VUEJSX插件
+    vue(),
+    vueJsx(),
     VueSetupExtend(),
     UnoCSS(),
-    dts({rollupTypes: true}),
-    DefineOptions()
+    dts({ rollupTypes: true }),
+    DefineOptions() as any,
   ],
   server: {
     host: '0.0.0.0',
     port: 8444,
     open: false,
-    https: false,
+    https: false as unknown as ServerOptions['https'], // 显式指定类型
   },
   css: {
     preprocessorOptions: {
-      scss:{
+      scss: {
         additionalData: `@import "./styles/global.scss";`
       }
     },
   },
-  // 添加库模式配置
-
   build: {
     target: 'modules',
     rollupOptions,
-    minify: 'terser', // boolean | 'terser' | 'esbuild'
-    // cssCodeSplit: true, // 追加css代码分割
-    sourcemap: false, // 生成代码源文件映射文件
-    reportCompressedSize: true, // 生成压缩大小报告
+    minify: 'terser',
+    sourcemap: false,
+    reportCompressedSize: true,
     lib: {
       entry: './src/entry.ts',
       name: 'FitUI',
       fileName: 'fitui',
-      // 导出模块格式
-      // formats: ['es', 'cjs'],
     },
     outDir: './dist',
   },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
-    // 支持tsx组件，很关键
-    transformMode: {
-      web: [/.[tj]sx$/],
-    },
-  },
-} as UserConfig
+  // test: {
+  //   globals: true,
+  //   environment: 'happy-dom',
+  //   transformMode: {
+  //     web: [/.[tj]sx$/],
+  //   },
+  // },
+}
 
 export default defineConfig(config)
