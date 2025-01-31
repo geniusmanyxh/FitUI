a5265e9c47242c9e2f22fc44a510b0ef201a7470
geniusmanyxh <geniusmanyxh@users.noreply.github.com>
2025-01-31 16:06:01 +0000

    deploy: 789ecfab13efb51a59ae5e7ec785681c3b5498b0

---
+++ assets/chunks/index.CPPpF_17.js
@@ -0,0 +1,1 @@
+import{F as e}from"./theme.C0O4XPYu.js";import{d as o,o as s,c as t,G as c,k as a,_ as d}from"./framework.BCXRjuC1.js";const n={class:"vitepress-codeblock"},r=o({__name:"index",setup(l){return(_,i)=>(s(),t("div",n,[c(a(e),{title:"示例代码",code:"console.log('Hello, World!');",lang:"js",showCode:!0,theme:"light"})]))}}),f=d(r,[["__scopeId","data-v-1bded035"]]);export{f as default};
