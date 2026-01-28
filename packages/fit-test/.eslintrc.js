409097c6975715effb3fcaac28e8dec2cf063f1a
gcodexhy <gcodexhy@gmail.com>
2026-01-28 23:47:34 +0800

    refactor: 重构项目

--- packages/fit-test/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-  extends: [
-    'plugin:json/recommended',
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/prettier',
-    './.eslintrc-auto-import.json'
-  ],
-}
